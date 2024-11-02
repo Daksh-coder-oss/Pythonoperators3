b339bcb82b16e044f2783a689c594f370ad85d41
Daksh-coder-oss <vv0039190@gmail.com>
2024-11-02 11:18:28 +0000

    Add files via upload

---
+++ Identity.py
@@ -0,0 +1,28 @@
+a=20
+if type(a)is int:
+    print("True")
+else:
+    print("False")    
+
+
+
+
+
+b=6.6
+if type(b)is not float:
+    print(True)
+else:
+    print(False)    
+
+c=20
+d=20
+if c is d:
+    print(True)
+else:
+    print(False)  
+
+d=30
+if c is not d:
+    print("Both are different")
+else:
+    print("They are the same")    
---
+++ average.py
@@ -0,0 +1,24 @@
+Maths=24
+Science=98
+English=87
+avg=(Maths+Science+English)/3
+if avg>=91 and avg<=100:
+    print("Your grade is A1")
+elif avg>=81 and avg<=90:
+    print("Your grade is A2")
+elif avg>=71 and avg<=80:
+    print("Your grade is B1")
+elif avg>=61 and avg<=70:
+    print("Your grade is B2")
+elif avg>=51 and avg<=60:
+    print("Your grade is C1")       
+elif avg>=41 and avg<=50:
+    print("Your grade is C2")    
+elif avg>=31 and avg<=40:
+    print("Your grade is D")
+elif avg>=0 and avg<=30:
+    print("Your grade is E")
+else:
+    print("Invalid")        
+
+            
